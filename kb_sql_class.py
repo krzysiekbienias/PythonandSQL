16607b9298b6230280d9cbb7557ef4d46503d98f
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-05-18 09:11:06 +0200

    Here I have presented how to connect Python with SQL server and creata dataframe object

---
+++ kb_sql_class.py
@@ -0,0 +1,123 @@
+import mysql.connector
+import numpy as np
+import pandas as pd
+import pymysql
+import sqlalchemy
+from os import listdir
+import os
+from typing import List
+
+
+
+class DataCollector():
+    def __init__(self,as_index,query):
+
+        self._as_index=as_index
+        self._query=query
+        self.m_connection_details = self.set_connection_details()
+
+        self.m_db_cursor = self.m_connection_details.cursor()
+        self.mdf_from_query=self.execute_query()
+
+
+    def set_connection_details(self):
+         return mysql.connector.connect(host="127.0.0.1",
+                                 user="root",
+                                 passwd="Numeraire2019",
+                                 database='sql_store')
+
+    def execute_query(self)->pd.DataFrame:
+        self.m_db_cursor.execute(self._query)
+        self.m_db_cursor.fetchall()
+        converted_df = pd.read_sql(self._query, con=self.m_connection_details)
+        final_df = converted_df.set_index(self._as_index)
+        return final_df
+
+class ExportDataFrame():
+    def __init__(self,input_path,suffix):
+        self._input_path=input_path
+        self._suffix=suffix
+        self.mls_whole_name=self.long_excel_names()
+        self.mls_short_names=self.short_excel_filenames()
+        self.mls_tabnames=self.get_tab_excel_list()
+        self.mdic_files_and_tabs=self.create_dictionary()
+        self.engine = sqlalchemy.create_engine('mysql+pymysql://root:Numeraire2019@127.0.0.1:3306/sql_store')
+
+
+
+
+
+
+    def long_excel_filenames(self):
+        return list(filter(lambda x: self._suffix in x, os.listdir(self._input_path)))
+
+    def short_excel_filenames(self):
+        ls_whole_names=list(filter(lambda x: self._suffix in x, os.listdir(self._input_path)))
+        ls_short_names=[]
+        for i in range(len(ls_whole_names)):
+            s_temp=ls_whole_names[i]
+            s_ticker=s_temp[:-4]
+            ls_short_names.append(s_ticker)
+        return ls_short_names
+
+    def get_tab_excel_list(self):
+        tab_list=[]
+        for i in range(len(self.mls_whole_name)):
+            xlsx=pd.ExcelFile(self.mls_whole_name[i])
+            ls_file_sheets=xlsx.sheet_names
+            tab_list.append(ls_file_sheets)
+        return tab_list
+
+    def create_dictionary(self):
+        files_names_and_tabs=dict(zip(self.mls_short_names,self.mls_tab_names))
+        return files_names_and_tabs
+
+
+
+
+
+    def export_data_frame(self,data_frame:pd.DataFrame,table_name:str):
+        return data_frame.to_sql(name=table_name, con=self.engine, index=True, if_exists='append')
+
+
+class CreateDataFrame:
+    def __init__(self,file_name:str,sheet_name:str,set_index=None):
+        self._file_name=file_name
+        self._sheet_name=sheet_name
+        self._set_index=set_index
+        self.mdf=self.create_data_frame_from_excel()
+
+    def create_data_frame_from_excel(self):
+        df_from_excel=pd.read_excel(self._file_name,sheet_name=self._sheet_name)
+        if self._set_index!=None:
+            df_from_excel=pd.read_excel(self._file_name,sheet_name=self._sheet_name)
+            return df_from_excel.set_index(self._set_index)
+        else:
+            return df_from_excel
+
+    def get_columns(self):
+        return self.mdf.columns
+
+
+if __name__=="__main__":
+
+
+    dc_check=DataCollector(as_index='order_id',
+                           query='''select  order_id,
+                              order_date,
+                              first_name,
+                              last_name,
+                              name as status
+                              from customers c
+                              join orders o 
+                              on o.customer_id=c.customer_id 
+                              join order_statuses os
+                              on o.status=os.order_status_id''')
+
+    df_order=dc_check.execute_query()
+
+
+
+
+
+print('The end of program')
