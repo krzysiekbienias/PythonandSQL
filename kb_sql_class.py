aa96305deaa1425fa91578eb68f7beaa3674820d
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-08-20 09:41:58 +0200

    Here I have presented how to connect Python with SQL server and creata dataframe object

--- kb_sql_class.py
+++ kb_sql_class.py
@@ -6,17 +6,19 @@ import sqlalchemy
 from os import listdir
 import os
 from typing import List
+from six import string_types
 
 
 
 class SQLConnector():
-    def __init__(self,query, as_index=None):
-        self._as_index = as_index
-        self._query = query
+    def __init__(self):
+
+
         self.m_connection_details = self.set_connection_details()
         self.m_db_cursor = self.m_connection_details.cursor()
-        self.mdf_from_query = self.execute_query()
+        #self.mdf_from_query = self.execute_query()
         self.engine = sqlalchemy.create_engine('mysql+pymysql://root:Numeraire2019@127.0.0.1:3306/sql_store')
+        self.df=self.testQuery(date='2014-05-19')
 
     def set_connection_details(self):
         return mysql.connector.connect(host="127.0.0.1",
@@ -24,15 +26,26 @@ class SQLConnector():
                                        passwd="Numeraire2019",
                                        database='sql_store')
 
-    def execute_query(self) -> pd.DataFrame:
-        self.m_db_cursor.execute(self._query)
-        self.m_db_cursor.fetchall()
-        converted_df = pd.read_sql(self._query, con=self.m_connection_details)
-        if self._as_index!=None:
-            final_df = converted_df.set_index(self._as_index)
-        else:
-            final_df=converted_df
-        return final_df
+    def quote_sql_string(self,value):
+        '''
+        If `value` is a string type, escapes single quotes in the string
+        and returns the string enclosed in single quotes.
+        '''
+        if isinstance(value, string_types):
+            new_value = str(value)
+            new_value = new_value.replace("'", "''")
+            return "'{}'".format(new_value)
+        return value
+
+
+    def testQuery(self,date=None):#example of dynamic query with fstrings
+        query=f'''SELECT * FROM sql_store.usd3mlibor
+                where observation_date='{date}' '''
+        df=pd.read_sql(query, con=self.m_connection_details)
+        return df
+
+
+
 
     def export_data_frame(self, data_frame: pd.DataFrame, table_name: str) -> pd.DataFrame:
         return data_frame.to_sql(name=table_name, con=self.engine, index=True, if_exists='append')
--- main.py
+++ main.py
@@ -13,34 +13,29 @@ from excelconnector import ExcelFilesDetails, CreateDataFrame
 
 
 if __name__ == "__main__":
-    dc_check = SQLConnector(as_index='order_id',
-                            query='''select  order_id,
-                              order_date,
-                              first_name,
-                              last_name,
-                              name as status
-                              from customers c
-                              join orders o 
-                              on o.customer_id=c.customer_id 
-                              join order_statuses os
-                              on o.status=os.order_status_id''')
+    dc_check = SQLConnector()
 
-    df_order = dc_check.execute_query()
 
-    efd = ExcelFilesDetails(input_path=data_path, suffix='.xls')
-    print(efd.mdic_files_and_tabs)
 
+
+
+#     efd = ExcelFilesDetails(input_path=data_path, suffix='.xls')
+#     print(efd.mdic_files_and_tabs)
+#
+# #####################################################################################################
+#     cdf = CreateDataFrame(file_name='to_paste.xls', sheet_name='murders')
+#     b_modify_before_insert=False #check value
+#     if b_modify_before_insert==True:
+#         dftoinsert = cdf.modify_columns_data_frame(columns_name='Company Name', l_fill_in='GOOG')
+#     else:
+#         dftoinsert=cdf.mdf
 #####################################################################################################
-    cdf = CreateDataFrame(file_name='to_paste.xls', sheet_name='murders')
-    b_modify_before_insert=False #check value
-    if b_modify_before_insert==True:
-        dftoinsert = cdf.modify_columns_data_frame(columns_name='Company Name', l_fill_in='GOOG')
-    else:
-        dftoinsert=cdf.mdf
-#####################################################################################################
-    b_insert_ornor = True #check value
-    if b_insert_ornor == True:
-        dc_check.export_data_frame(dftoinsert, table_name='murders')
+    # b_insert_ornor = False #check value
+    # if b_insert_ornor == True:
+    #     dc_check.export_data_frame(dftoinsert, table_name='murders')
+
+
+
 
     dc_check.close_conection()
     print('The end of program')
