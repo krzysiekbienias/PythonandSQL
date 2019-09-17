79a43a4065341391db876aa2c6ffcf502e56d8ca
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2019-09-17 21:54:35 +0200

    Here I have presented how to connect Python with SQL server and creata dataframe object

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ from excelconnector import ExcelFilesDetails, CreateDataFrame
 if __name__ == "__main__":
     dc_check = SQLConnector()
 
-    data_path = '/Users/krzysiekbienias/Documents/ExcelDataStore/GenericStore'
+    data_path = '/Users/krzysiekbienias/Documents/ExcelDataStore/EquityPortfolio'
 
     os.chdir(data_path)
     excel_details = ExcelFilesDetails(input_path=data_path, suffix='.xls')
+    print(f'W have the following Excel Files {excel_details.long_excel_filenames()}')
+    print(f'W have the dictionaries Excel Files {excel_details.create_dictionary()}')
+    #In rows we have files and cells are tabs
+    dfOfExcel=pd.DataFrame.from_dict(excel_details.create_dictionary(),orient='index')
+
 
     dc_check.close_conection()
     print('The end of program')
